b1c1096110245041594255e9c3e070a6e960ff5d
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-17 18:38:59 +0100

    Stampati a schermo i post | Esercizio completo

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -6,9 +6,10 @@
   </head>
   <body>
     <div class="sass">
+      
       @foreach ($posts as $post)
         <h1>{{$post->title}}</h1>
-
+        <p>{{$post->body}}</p>
       @endforeach
 
     </div>
